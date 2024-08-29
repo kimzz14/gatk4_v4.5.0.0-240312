53141f178107926af833b319fbc861b9bf05edfd
kimzz14 <kimzz1004@gmail.com>
2024-08-29 09:53:02 +0900

    edit

--- pipe/GATK-SplitNCigarReads.sh
+++ pipe/GATK-SplitNCigarReads.sh
@@ -4,6 +4,7 @@ readID=$1
 ############################################################################################
 
 gatk --java-options "-Djava.io.tmpdir=./tmp" SplitNCigarReads \
+--create-output-bam-index false \
 --reference reference/ref.fa \
 --input     result/${readID}.bam \
 --output    result/${readID}.SplitN.bam \
@@ -11,3 +12,9 @@ gatk --java-options "-Djava.io.tmpdir=./tmp" SplitNCigarReads \
 2>          result/${readID}.SplitN.bam.err
 
 #rm         result/${readID}.bam
+#add 2024.08.29 --create-output-bam-index false
+
+samtools index \
+            result/${readID}.SplitN.bam \
+1>          result/${readID}.SplitN.bam.bai.log \
+2>          result/${readID}.SplitN.bam.bai.err
